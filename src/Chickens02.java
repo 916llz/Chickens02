18b8c18eba6e655f8afacca51b7e56de28d82224
ALUNO <ALUNO@10.85.198.5>
2024-06-04 16:05:41 -0300

    Commit project Chickens02

---
+++ src/Chickens02.java
@@ -0,0 +1,15 @@
+public class Chickens02 {
+
+    public static void main(String[] args) {
+        //Put yout code here
+        int mediadiaria, mediamensal, lucromensal;
+        mediadiaria = (int) 112.6666666666667;
+        mediamensal = (int) 3380.0;
+        lucromensal = (int) 608.8;
+        
+        System.out.println("Daily Average:   " +mediadiaria);
+        System.out.println("Monthly Average: "  +mediamensal);
+        System.out.println("Monthly Profit:  $" +lucromensal);
+    }
+    
+}
